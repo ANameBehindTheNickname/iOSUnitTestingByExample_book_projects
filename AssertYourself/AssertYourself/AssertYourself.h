7775c57c5fef24c67ac77a8157ed4dff7899d8f0
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2022-02-07 11:29:43 +0200

    Add AssertYourself project

---
+++ AssertYourself/AssertYourself/AssertYourself.h
@@ -0,0 +1,16 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssertYourself.
+FOUNDATION_EXPORT double AssertYourselfVersionNumber;
+
+//! Project version string for AssertYourself.
+FOUNDATION_EXPORT const unsigned char AssertYourselfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssertYourself/PublicHeader.h>
+
+
