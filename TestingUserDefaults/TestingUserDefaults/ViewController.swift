187f7e5283e45a88f9b149d03cb8f5c1ef82ff57
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2022-02-08 15:27:16 +0200

    Introduce a difficult dependency - UserDefaults

--- TestingUserDefaults/TestingUserDefaults/ViewController.swift
+++ TestingUserDefaults/TestingUserDefaults/ViewController.swift
@@ -6,12 +6,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private(set) var counterLabel: UILabel!
+    @IBOutlet private(set) var incrementButton: UIButton!
+    
+    private var count = 0 {
+        didSet {
+            counterLabel.text = "\(count)"
+            UserDefaults.standard.set(count, forKey: "count")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        count = UserDefaults.standard.integer(forKey: "count")
     }
 
-
+    @IBAction private func incrementButtonTapped() {
+        count += 1
+    }
 }
 
