f2a4aa481dbacdaf6d25f30e32489973aeb241e6
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2022-02-08 12:16:08 +0200

    Add ManageDifficultDependencies project

---
+++ ManageDifficultDependencies/ManageDifficultDependencies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ManageDifficultDependencies/ManageDifficultDependenciesTests/ManageDifficultDependenciesTests.swift
@@ -0,0 +1,11 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+import XCTest
+@testable import ManageDifficultDependencies
+
+class ManageDifficultDependenciesTests: XCTestCase {
+
+}
