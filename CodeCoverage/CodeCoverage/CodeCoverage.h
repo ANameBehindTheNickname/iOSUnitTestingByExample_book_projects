aa860557b3036e87a3b980b9fae747901d842107
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2022-02-08 09:29:45 +0200

    Add CodeCoverage project

---
+++ CodeCoverage/CodeCoverage/CodeCoverage.h
@@ -0,0 +1,16 @@
+//
+//
+//  Copyright © 2022 ANameBehindTheNickname. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeCoverage.
+FOUNDATION_EXPORT double CodeCoverageVersionNumber;
+
+//! Project version string for CodeCoverage.
+FOUNDATION_EXPORT const unsigned char CodeCoverageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeCoverage/PublicHeader.h>
+
+
