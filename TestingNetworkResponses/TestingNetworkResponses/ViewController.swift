91b1bd00b8ad3c2286c69372b958384660f3aa88
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2022-02-09 15:26:49 +0200

    Add a method for searching the book

--- TestingNetworkResponses/TestingNetworkResponses/ViewController.swift
+++ TestingNetworkResponses/TestingNetworkResponses/ViewController.swift
@@ -6,12 +6,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private(set) var button: UIButton!
+    private var dataTask: URLSessionDataTask?
+    
+    @IBAction private func buttonTapped() {
+        searchForBook(terms: "out from boneville")
     }
-
-
+    
+    private func searchForBook(terms: String) {
+        guard let encodedTerms = terms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "https://itunes.apple.com/search?" +
+                            "media=ebook&term=\(encodedTerms)") else { return }
+        
+        let request = URLRequest(url: url)
+        dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            let decoded = String(data: data ?? Data(), encoding: .utf8)
+            print("response: \(String(describing: response))")
+            print("data: \(String(describing: decoded))")
+            print("error: \(String(describing: error))")
+            
+            DispatchQueue.main.async {
+                self.dataTask = nil
+                self.button.isEnabled = true
+            }
+        }
+        
+        button.isEnabled = false
+        dataTask?.resume()
+    }
 }
 
